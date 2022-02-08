3f2c7f24e523af134862f65c68293b2b570bd9c3
Gabriel Valle <you@example.comgabrielvallerodrigues@gmail.com>
2022-02-08 11:02:32 -0300

    iniciando curso

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Agora deu certo</h1>
+    </div>
+  );
+}
+
+export default App;

cf8ab8bf49b79eccce01097e222dfcd2586ccb30
Gabriel Valle <you@example.comgabrielvallerodrigues@gmail.com>
2022-02-09 14:09:57 -0300

    Finalizado introdução

--- my-app/src/components/Evento.js
+++ my-app/src/components/Evento.js
@@ -1,14 +1,20 @@
-
+import Button from "./Button"
 function Evento(){
 
     function meuEvento(){
         console.log("Opa, fui ativado!!")
     }
+    function segundoEvento(){
+        console.log("Segundo evento fui ativado!!")
+    }
 
     return (
         <>
+        
         <p> Clique para desparar um evento</p>
-        <button onClick={meuEvento}>Ativar!</button>
+        <button onClick={meuEvento}>Ativando primeiro evento!</button>
+        <Button event={meuEvento} text="Primeiro Evento"/>
+        <Button event={segundoEvento} text="Segundo Evento"/>
         </>
     )
 }
